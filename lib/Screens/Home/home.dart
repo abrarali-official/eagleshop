6c6f5e549f597b367af26579a6cce682c7a0b964
S-Abbdullah <98264416+S-Abbdullah@users.noreply.github.com>
2022-04-05 03:10:09 +0500

    gg

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -1,25 +1,117 @@
+import 'package:fancy_drawer/fancy_drawer.dart';
 import 'package:flutter/material.dart';
-
 import '../../Widgets/Custom/customnav.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  late FancyDrawerController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = FancyDrawerController(
+        vsync: this, duration: const Duration(milliseconds: 250))
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      bottomNavigationBar: const CustomNavbar(),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(50),
+      child: Scaffold(
+        appBar: AppBar(),
+        drawer: FancyDrawerWrapper(
+          backgroundColor: Colors.white,
+          controller: _controller,
+          drawerItems: <Widget>[
+            Text(
+              "Go to home",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.purple.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "About us",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.purple.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Our products",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.purple.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Support us",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.purple.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Log out",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.purple.shade700,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+          child: Scaffold(
+            appBar: AppBar(
+              elevation: 4.0,
+              title: const Text(
+                "Some appbar",
+                style: TextStyle(color: Colors.black),
+              ),
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  _controller.toggle();
+                },
+              ),
+            ),
+            body: const Center(
+              child: Text("Body"),
+            ),
           ),
         ),
-        onPressed: () {},
-        child: const Icon(Icons.add_shopping_cart_rounded),
+        bottomNavigationBar: const CustomNavbar(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(50),
+            ),
+          ),
+          onPressed: () {},
+          child: const Icon(Icons.add_shopping_cart_rounded),
+        ),
       ),
-    ));
+    );
   }
 }
